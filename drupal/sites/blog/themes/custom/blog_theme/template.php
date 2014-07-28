bb27340979c8e0475ef330167ac6fc2ec48b433b
keyral <simon.raynaud42@gmail.com>
2014-07-28 19:10:53 +0200

    add custom template for blog_contact form

--- drupal/sites/blog/themes/custom/blog_theme/template.php
+++ drupal/sites/blog/themes/custom/blog_theme/template.php
@@ -4,9 +4,24 @@
  *
  */
 
-
+/**
+ * Implements hook_preprocess_node()
+ */
 function blog_theme_preprocess_node(&$vars) {
     if($vars['node']) {
         $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__'.$vars['view_mode'];
     }
+}
+
+/**
+ * Implements hook_theme()
+ */
+function blog_theme_theme($existing, $type, $theme, $path) {
+    $items = array();
+    // Custom template to form contact
+    $items['blog_form_contact'] = array(
+        'render element' => 'form',
+        'path' => drupal_get_path('module', 'blog_contact') . '/templates/form--contact',
+    );
+    return $items;
 }
